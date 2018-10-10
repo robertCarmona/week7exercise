4bfe382010e7c565646fe3ba29d414bc052869d9
Robert Carmona <Robert@Roberts-MacBook-Pro-2.local>
2018-10-10 13:45:06 -0400

    added a input and button where input is display as title

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("connected");
+console.log("sucess")
+var h1 = document.getElementById("h1element");
+h1.innerText = "hello";
+
+var showText = document.getElementById("showText");
+function displayH3() {
+  h1.innerText = showText.value;
+  console.log(h1.innerText)
+}
